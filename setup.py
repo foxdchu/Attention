22e58773822e40e07c14f1cb2eb7fdb3f7162ac7
foxdchu <47610780+foxdchu@users.noreply.github.com>
2019-11-25 19:59:58 -0800

    Attention files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name="Attention", 
+    version="0.1",
+    author="Mark Rabins",
+    description="A small package for Mark",
+    packages=['Attention'],
+    requires=['tensorflow']
+)
